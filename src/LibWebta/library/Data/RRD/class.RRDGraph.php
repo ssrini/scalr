d51027a9472eaaab7994e46d09e9b87c978db0da
Srinivasan Subramanian <srinivasan.subramanian@gmail.com>
2012-05-09 16:50:37 +0530

    Renamed RRDGraph to RRDGraphScalr to avoid the class RRDGraph redeclared error.

--- src/LibWebta/library/Data/RRD/class.RRDGraph.php
+++ src/LibWebta/library/Data/RRD/class.RRDGraph.php
@@ -30,7 +30,7 @@
      * @version 1.0
      * @author Igor Savchenko <http://webta.net/company.html>
      */
-	class RRDGraph extends Core
+	class RRDGraphScalr extends Core
 	{
 	    /**
 	     * RRD Graph width
